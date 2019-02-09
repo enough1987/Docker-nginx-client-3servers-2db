4bbbc1e3ace361cc5502959e086b54b00ebbf2ec
otilha <otilha@corelogic.com>
2019-02-10 01:35:30 +0800

    add long running request

--- server1/routes/index.js
+++ server1/routes/index.js
@@ -3,9 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({
-    name: "server 1 is running very good"
-  });
+  setTimeout(() => {
+    res.json({
+      name: "server 1 is running very good"
+    });
+  }, 60000);
 });
 
 module.exports = router;
